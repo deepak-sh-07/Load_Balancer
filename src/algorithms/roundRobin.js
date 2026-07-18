c8561e45c7b60ad610f1fcfe21b6761fbd7363b5
Deepak Sharma <deepakshar661@gmail.com>
2026-07-18 21:14:32 +0530

    Changes

--- src/algorithms/roundRobin.js
+++ src/algorithms/roundRobin.js
@@ -1,7 +1,7 @@
+let current = 0;
 
-let count = 0;
-export function Roundrobin() {
-    count++;
-    return count % 3;
-
+export function getNextServer(servers) {
+    const server = servers[current];
+    current = (current + 1) % servers.length;
+    return server;
 }
--- src/index.js
+++ src/index.js
@@ -1,44 +1,21 @@
 import express from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
+import { getNextServer } from './algorithms/roundRobin'
 const app = express()
 const PORT = 3000
+import config from "../config.json" assert { type: "json" };
+const servers = config.servers;
 
 app.use(express.json())
-const servers = [
-    "http://localhost:5001",
-    "http://localhost:5002",
-    "http://localhost:5003"
-];
-
-let current = 0;
-
-function getNextServer() {
-    const server = servers[current];
-    current = (current + 1) % servers.length;
-    return server;
-}
 
 app.use((req, res, next) => {
-    const target = getNextServer();
-
     createProxyMiddleware({
-        target,
-        changeOrigin: true,
-    })(req, res, next);
+    router: () => getNextServer(servers),
+    changeOrigin: true,
+    });
+    
 });
 
-// const server1 = createProxyMiddleware({
-//     target: "http://localhost:5001",
-//     changeOrigin: true,
-// })
-// const server3 = createProxyMiddleware({
-//     target: "http://localhost:5001",
-//     changeOrigin: true,
-// })
-// const server3 = createProxyMiddleware({
-//     target: "http://localhost:5003",
-//     changeOrigin: true,
-// })
 app.listen(PORT, () => {
     console.log(`Server is running ${PORT}`)
 })
